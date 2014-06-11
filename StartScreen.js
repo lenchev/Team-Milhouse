19dc7193ac76740af483600e5285f1b691b7de6e
gdamyanova <g_damyanova@mail.bg>
2014-06-11 13:28:19 +0300

    some images

--- StartScreen.js
+++ StartScreen.js
@@ -1,4 +1,19 @@
-﻿var canvas = document.createElement("canvas");
+﻿window.onload = function() {
+var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 
+ctx.font = "30px Arial";
+ctx.fillText("Hello World", 20, 20);
+    
+    draw_lame();
+
+    function draw_lame() {
+        var levelLame = new Image();
+        levelLame.src = "levelLame.png";
+        levelLame.onload = function () {
+            context.drawImage(levelLame, 10, 10);
+        }
+    }
+
+}
 
